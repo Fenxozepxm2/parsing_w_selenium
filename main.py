377958373df79da68ef7f3a352ec88c7c38578cf
Fenxozepxm2 <rip2046@mail.ru>
2026-04-02 00:55:09 +0700

    полная версия парсера сразу с выводом в 2 формата

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import csv
 import requests
 import json
@@ -25,38 +27,61 @@ from tqdm import tqdm
 # 5. обработку ошибок
 
 
-driver: WebDriver = webdriver.Chrome()
+service = Service(ChromeDriverManager().install())
+driver: WebDriver = webdriver.Chrome(service = service)
 driver.get("https://quotes.toscrape.com/js/")
 
 ua = UserAgent()
 headers = {'User-Agent': ua.random }
-qout_list = {}
+qout_list = []
 
 
 next_btn = driver.find_element(By.CLASS_NAME, 'next').find_element(By.TAG_NAME, 'a').get_attribute('href')
+#использовал только для личного удобства
+pbar = tqdm()
 
-
-while next_btn[:1]:
-
+while next_btn:
+    pbar.update(1)
     qouts = driver.find_elements(By.CLASS_NAME, 'quote')
     for qout in qouts:
+        #парсиг данных
         try:
-            qoute_text = driver.find_element(By.CLASS_NAME, 'text').text
+            qoute_text = qout.find_element(By.CLASS_NAME, 'text').text
         except Exception:
             qoute_text = 'None'
-        
         try:
-            qoute_author = driver.find_element(By.CLASS_NAME, 'author').text
+            qoute_author = qout.find_element(By.CLASS_NAME, 'author').text
+        except Exception:
+            qoute_author = 'None'
+        try:
+            qoute_tags = qout.find_element(By.CLASS_NAME, 'tags').find_elements(By.CSS_SELECTOR, 'a')
+            tags_text = [tag.text for tag in qoute_tags]
         except Exception:
             qoute_text = 'None'
 
-        try:
-            qoute_tags = driver.find_element(By.CLASS_NAME, 'text').text
-        except Exception:
-            qoute_text = 'None'
-
+        qout_list.append({
+            'qoute_text': qoute_text,
+            'qoute_author': qoute_author,
+            'tags': tags_text
+        })
     time.sleep(0.5)
-    next_btn = driver.find_element(By.CLASS_NAME, 'next').find_element(By.TAG_NAME, 'a').get_attribute('href')
-    driver.get(next_btn)
+    try:
+        #не клик, потому что ширина высота кнопки равняется 0, кнопка не может быть нажата методом click()
+        next_btn = driver.find_element(By.CLASS_NAME, 'next').find_element(By.TAG_NAME, 'a').get_attribute('href')
+        driver.get(next_btn)
+    except Exception:
+        break
 
+#запись сразу в два удобных формата
+with open('all_qouts.json', 'w', encoding='utf-8') as file:
+    json.dump(qout_list, file, indent=4, ensure_ascii=False)
+
+with open('all_qouts.csv', 'w', encoding='utf-8-sig', newline='') as f:
+    writer = csv.writer(f, delimiter=';')
+    writer.writerow(['Text', 'Author', 'Теги'])
+    for q in qout_list:
+        tags_str = ', '.join(q['tags'])
+        writer.writerow([q['qoute_text'], q['qoute_author'], tags_str])
+
+#ожидание ввода, что бы драйвер браузера не закрывался сразу
 input()
